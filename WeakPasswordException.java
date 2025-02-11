467670608e543d6eef721ffca9b3a9ba96287d4b
uumunna21 <uumunna@montgomerycollege.edu>
2025-02-11 18:34:02 -0500

    Delete WeakPasswordException.java

--- WeakPasswordException.java
+++
@@ -1,6 +0,0 @@
-
-public class WeakPasswordException extends Exception {
-    public WeakPasswordException() {
-        super("The password is OK but weak - it contains fewer than 10 characters");
-    }
-}
